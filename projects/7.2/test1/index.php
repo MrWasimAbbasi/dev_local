b5744f3e54d8d57aa80bf2ade7ddf05b881586af
MWaseemAbbasi <mwaseem@tekambi.com>
2024-08-23 19:35:20 +0500

    initial commit

---
+++ projects/7.2/test1/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h1>Hello, From Project test1. <span style='color:red'> PHP_VERSION: ". PHP_VERSION . "</span></h1>";
+
+
+
+$servername = "mysql-5.6"; // Use the service name as the host
+$username = "root";
+$password = "root";
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully with database";
---
+++ projects/8.3/test1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<h1>Hello, From Project test1. <span style='color:red'> PHP_VERSION: ". PHP_VERSION . "</span></h1>";
+
+$servername = "mysql-8.0"; // Use the service name as the host
+$username = "root";
+$password = "root";
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully with database";
